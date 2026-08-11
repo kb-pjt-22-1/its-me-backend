3cf9efd64ad5b7271cb451826488936d982536c1
ksy <ksy9565@gmail.com>
2026-08-11 17:28:13 +0900

    fix: MyBatisConfig.java의 매퍼스캔 어노테이션에 누락된 코드 수정

--- src/main/java/site/benepay/common/config/MyBatisConfig.java
+++ src/main/java/site/benepay/common/config/MyBatisConfig.java
@@ -1,5 +1,7 @@
 package site.benepay.common.config;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -8,39 +10,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import javax.sql.DataSource;
-
 @Configuration
 @MapperScan(basePackages = {
-        "site.benepay.domain.user.mapper",
-        "site.benepay.domain.card.mapper",
-        "site.benepay.domain.merchant.mapper",
-        "site.benepay.domain.payment.mapper",
-        "site.benepay.domain.recommendation.mapper",
-        "site.benepay.common.crypto.mapper"
+	"site.benepay.domain.user.mapper",
+	"site.benepay.domain.card.mapper",
+	"site.benepay.domain.merchant.mapper",
+	"site.benepay.domain.bookmark.mapper",
+	"site.benepay.domain.payment.mapper",
+	"site.benepay.domain.recommendation.mapper",
+	"site.benepay.common.crypto.mapper"
 })
 public class MyBatisConfig {
 
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setMapperLocations(
-                new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml"));
-        factoryBean.setTypeAliasesPackage("site.benepay.domain.user.vo");
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(dataSource);
+		factoryBean.setMapperLocations(
+			new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml"));
+		factoryBean.setTypeAliasesPackage("site.benepay.domain.user.vo");
 
-        // snake_case → camelCase 자동 매핑 설정
-        org.apache.ibatis.session.Configuration mybatisConfiguration =
-                new org.apache.ibatis.session.Configuration();
+		// snake_case → camelCase 자동 매핑 설정
+		org.apache.ibatis.session.Configuration mybatisConfiguration =
+			new org.apache.ibatis.session.Configuration();
 
-        mybatisConfiguration.setMapUnderscoreToCamelCase(true);
+		mybatisConfiguration.setMapUnderscoreToCamelCase(true);
 
-        factoryBean.setConfiguration(mybatisConfiguration);
-        return factoryBean.getObject();
-    }
+		factoryBean.setConfiguration(mybatisConfiguration);
+		return factoryBean.getObject();
+	}
 
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
 }
